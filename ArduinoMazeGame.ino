59769225227b84c9a25614fd88b24c840003d153
Akash Nayar <burgercheetah5211@gmail.com>
2020-12-05 21:06:06 -0500

    Cleaned up unused code

--- ArduinoMazeGame.ino
+++ ArduinoMazeGame.ino
@@ -1,5 +1,3 @@
-// Basic demo for accelerometer/gyro readings from Adafruit LSM6DS33
-
 #include <Adafruit_LSM6DS33.h>
 #include <Adafruit_NeoMatrix.h>
 
@@ -7,14 +5,9 @@
 #define BLUE 31
 #define BLACK 1
 #define DELAY 250
-
-
 using namespace std;
 
-
-// For SPI mode, we need a CS pin
 #define LSM_CS 10
-// For software-SPI mode we need SCK/MOSI/MISO pins
 #define LSM_SCK 13
 #define LSM_MISO 12
 #define LSM_MOSI 11
@@ -72,7 +65,6 @@ void setup(void) {
   matrix.begin();
   matrix.setTextWrap(false);
   matrix.setBrightness(40);
-  
 }
 
 int getY(int x, int y) {
@@ -84,29 +76,8 @@ int getY(int x, int y) {
   }
 }
 
-String getBoard(int x, int y) {
-
-  String boardStr = "";
-  for (int i = 0; i < 16; i++) {
-    for (int j = 0; j < 16; j++) {
-      if (pgm_read_word(&board[i][j]) == 1) {
-        boardStr += "X";
-      }
-      else if (j == x && i == y) {
-        boardStr += "O";
-      }
-      else {
-        boardStr += " ";
-      }
-    }
-    boardStr += "\r\n";
-  }
-  return boardStr;
-}
-
 void loop() {
-  //  /* Get a new normalized sensor event */
-
+  // get new normalized sensor event
   sensors_event_t accel;
   sensors_event_t gyro;
   sensors_event_t temp;
@@ -115,16 +86,13 @@ void loop() {
   float accel_y = accel.acceleration.y;
 
   // postive x is right, positive y is up
-  // threshold is 2
-  // 0 = up, 1 = right, 2 = down, 3 = left
+  // threshold is 2 radians
 
   int old_x = x;
   int old_y = y;
 
-
   if (accel_x > 2 | accel_x < -2) {
     if (accel_x > 0) {
-      // right
       if (x > 0) {
         x--;
       }
@@ -137,13 +105,11 @@ void loop() {
   }
   else if (accel_y > 2 | accel_y < -2) {
     if (accel_y > 0) {
-      // left
       if (y > 0) {
         y--;
       }
     }
     else {
-      //Serial.print("down");
       if (y < 15) {
         y++;
       }
@@ -151,59 +117,35 @@ void loop() {
   }
 
   // check to see if board[y][x] is 0
-
   if (pgm_read_word(&board[y][x]) == 1) {
     y = old_y;
     x = old_x;
   }
-
-  Serial.write(27);
-  Serial.print(F("[2J"));
-  Serial.write(27);
-  Serial.print(F("[H"));
 
   // check to see if it's a win
   if (x == 14 &&  getY(x, y) == 15){
     matrix.setCursor(0, 0);
     matrix.fillScreen(0);
     matrix.print(F("You win!"));
+    win = true;
   }
 
-
-  if (old_x != x | old_y != y and win != true) {
+  if ((old_x != x | old_y != y) and win != true) {
     // update board elements
-
-
-
-
     for (int i = 0; i < 16; i++) {
       for (int j = 0; j < 16; j++) {
-
         int item = pgm_read_word(&board[j][i]);
         if (item == 1) {
           matrix.drawPixel(j, getY(j, i), BLUE);
         }
-
         if (item == 0) {
           matrix.drawPixel(j, getY(j, i), BLACK);
         }
-
-
       }
     }
-
     matrix.drawPixel(y, getY(y, x), matrix.Color(255, 255, 255));
-    Serial.print(F("DRawing at: "));
-    Serial.print(x);
-    Serial.print(F(", "));
-    Serial.print(getY(x, y));
-    Serial.println(F(""));
   }
 
-
-  // clear serial monitor
-
-  Serial.print(getBoard(x, y));
   matrix.show();
   delay(DELAY);
 }
